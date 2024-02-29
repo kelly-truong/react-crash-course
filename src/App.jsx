6ec5afa7313a3ef44cfa6324a8ebfbd93500e372
Kelly <kellytruong6973@gmail.com>
2024-02-29 17:20:51 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css';
+import Todo from './components/Todo.jsx'
+import Title from './components/Title.jsx'
+import Modal from './components/Modal.jsx'
+import React, { useState } from 'react'
+import Counter from './components/Counter.jsx'
+
+function App() {
+  const [showModal, setShowModal] = useState(false)
+
+  function onTodoDelete() {
+    setShowModal(true)
+  }
+
+  function cancelModal() {
+    setShowModal(false)
+  }
+
+  function confirmModal() {
+    setShowModal(false)
+  }
+
+  return (
+    <div>
+      <Title />
+      <div>
+        <input type="text" onChange={(event) => {
+          console.log(event.target.value)
+        }} />
+        <button>Add todo</button>
+      </div>
+      <div className="todo__wrapper">
+        <Todo
+          onTodoDelete={onTodoDelete}
+          title="Finish Frontend Simplified" />
+        <Todo
+          onTodoDelete={onTodoDelete}
+          title="Finish interview section" />
+        <Todo
+          onTodoDelete={onTodoDelete}
+          title="Land 100k job" />
+      </div>
+      {showModal && <Modal
+        cancelModal={cancelModal}
+        confirmModal={confirmModal}
+        title="Confirm delete?" />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,15 @@
+import './Todo.css'
+
+function Todo({title, onTodoDelete }) {
+    function deleteTodo(id) {
+        console.log('deleteTodo()', title.toUpperCase())
+    }
+    return (
+        <div className="todo">
+            <p>{title}</p>
+            <button onClick={onTodoDelete}>Delete </button>
+        </div>
+    )
+}
+
+export default Todo
